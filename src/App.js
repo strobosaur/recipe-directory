8cd7ff705e237e2fb23dcf641b5087f201334082
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-01-05 13:59:15 +0100

    basic page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// CSS STYLES
 import './App.css';
+
+// PAGE COMPONENTS
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipe/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
